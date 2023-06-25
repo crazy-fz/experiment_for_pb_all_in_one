e2579c18cb485772d0c9538af3134234e0ec34c5
crazy_fz <47535428+crazy-fz@users.noreply.github.com>
2023-06-26 00:03:17 +0800

    fix run.sh: export LD_LIBRARY_PATH before make

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ rm ./*.so
 rm ./*.o
 rm ./*.a
 rm ./*.out
-export LIBRARY_PATH=`pwd`:$LIBRARY_PATH
-make all
 export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
+make all
+export LIBRARY_PATH=`pwd`:$LIBRARY_PATH
 ./main.out
